5ca62d4204fe4040c095f80bc5f42dc14151acd8
save <eric@benitez.ca>
2022-02-24 04:03:40 -0500

    first commit

---
+++ qotrt/src/test/java/comp3004team6/qotrt/QuestsOfTheRoundTableApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3004team6.qotrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestsOfTheRoundTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
