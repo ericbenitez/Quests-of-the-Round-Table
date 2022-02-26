ea039f0bc4198684694405c39938d6a65d7e2452
Donna-Smee <65718414+Donna-Smee@users.noreply.github.com>
2022-02-25 23:47:02 -0500

    Adding classes for the adventure cards

---
+++ qotrt/src/main/java/comp3004team6/qotrt/Turn.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Turn {
+  String name;
+  boolean isQuest = false;
+  boolean isTournament = false;
+  boolean isEvent = false;
+
+  ArrayList<Player> participants;
+}
