00d13a29eddac69fe9fd27f64d1385322a4c9602
save <eric@benitez.ca>
2022-03-03 22:21:20 -0500

    test commit Co-authored-by: Donna Ha <Donna-Smee@users.noreply.github.com> Co-authored-by: Fareha Sultan <farehasultan@users.noreply.github.com> Co-authored-by: Ana Lian <alian1993@users.noreply.github.com>

---
+++ qotrt/src/main/java/Models/StoryCards/QuestBehaviour.java
@@ -0,0 +1,11 @@
+package Models.StoryCards;
+import java.util.ArrayList;
+
+import Models.General.*;
+
+
+public interface QuestBehaviour {
+
+    void playEvent(ArrayList<Player> players, Player drawer);
+  
+}
